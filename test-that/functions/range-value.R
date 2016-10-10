5e544b69302a301ca9456c221bef072d353bcc92
xiaoqian-zhu <xqzhu@berkeley.edu>
2016-10-10 10:37:29 -0700

    improved range-value

--- test-that/functions/range-value.R
+++ test-that/functions/range-value.R
@@ -1,7 +1,5 @@
-range_value <- function(x, na.rm=F) {
-  
-  if (na.rm) {
-    x <- na.omit(x)
-  }
+range_value <- function(x, na.rm = T){
+  if (na.rm == T){
+    x <- x[!is.na(x)]}
   max(x) - min(x)
 }
